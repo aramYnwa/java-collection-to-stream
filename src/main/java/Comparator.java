e51e04e6eccfda9a378bcabaa4d7360a95e4b415
Aramais Khachatryan <rtgkid8@gmail.com>
2020-10-18 16:46:17 +0400

    Making comparing method generic from Person type

--- src/main/java/Comparator.java
+++ src/main/java/Comparator.java
@@ -4,7 +4,7 @@ import java.util.function.Function;
 public interface Comparator<T> {
   public int compare(T t1, T t2);
 
-  public static Comparator<Person> comparing(Function<Person, Comparable> f) {
+  public static <U> Comparator<U> comparing(Function<U, Comparable> f) {
     return (p1, p2) -> f.apply(p1).compareTo(f.apply(p2));
   }
 }
