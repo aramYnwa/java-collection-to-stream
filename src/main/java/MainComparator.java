a671ac5a713799f0c3667e611347e5f04af88af5
Aramais Khachatryan <rtgkid8@gmail.com>
2020-10-18 15:57:43 +0400

    Added ignore files

--- src/main/java/MainComparator.java
+++ src/main/java/MainComparator.java
@@ -1,11 +1,7 @@
 public class MainComparator {
 
   public static void main(String[] args) {
-    Comparator<Person> comparator = new Comparator<Person>() {
-      @Override
-      public int compare(Person t1, Person t2) {
-        return 0;
-      }
-    };
+    Comparator<Person> cmpAge = (p1, p2) -> p1.getAge() - p2.getAge();
+    Comparator<Person> cmpFirstName = (p1, p2) -> p1.getFirstName().compareTo(p2.getFirstName());
   }
 }
