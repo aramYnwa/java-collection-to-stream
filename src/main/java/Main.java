97f5bd8b69f0cacf29e72da9872fa5f63896a4aa
Aramais Khachatryan <rtgkid8@gmail.com>
2020-10-18 17:59:44 +0400

    Implemented and functionality for Predicate interface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,12 @@ public class Main {
 
   public static void main(String[] args) {
 
+    Predicate<String> p1 = p -> p.length() < 20;
+    Predicate<String> p2 = p -> p.startsWith("A");
+    Predicate<String> p3 = p -> !p.isEmpty();
+
+    Predicate<String> p = p1.and(p2).and(p3);
+
+    System.out.println(p.test("Avatar"));
   }
 }
--- src/main/java/Predicate.java
+++ src/main/java/Predicate.java
@@ -1,4 +1,8 @@
 @FunctionalInterface
 public interface Predicate<T> {
   boolean test(T t);
+
+  default Predicate<T> and(Predicate<T> p2) {
+    return p1 -> test(p1) && p2.test(p1);
+  }
 }
