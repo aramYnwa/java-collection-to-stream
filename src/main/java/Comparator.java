1935e6502325148929d310d9443b00800be709e0
Aramais Khachatryan <rtgkid8@gmail.com>
2020-10-18 16:40:50 +0400

    Using comparing with function reference

--- src/main/java/Comparator.java
+++ src/main/java/Comparator.java
@@ -1,4 +1,10 @@
+import java.util.function.Function;
+
 @FunctionalInterface
 public interface Comparator<T> {
   public int compare(T t1, T t2);
+
+  public static Comparator<Person> comparing(Function<Person, Integer> f) {
+    return (p1, p2) -> f.apply(p1) - f.apply(p2);
+  }
 }
--- src/main/java/MainComparator.java
+++ src/main/java/MainComparator.java
@@ -1,7 +1,14 @@
+import java.util.function.Function;
+
 public class MainComparator {
 
   public static void main(String[] args) {
     Comparator<Person> cmpAge = (p1, p2) -> p1.getAge() - p2.getAge();
     Comparator<Person> cmpFirstName = (p1, p2) -> p1.getFirstName().compareTo(p2.getFirstName());
+
+    Function<Person, Integer> fAge = p -> p.getAge();
+    Function<Person, String> fFirstName = p -> p.getFirstName();
+
+    Comparator<Person> cmpAgeFunc = Comparator.comparing(Person::getAge);
   }
 }
