be2ee11864487eade1b79d3917574630df7b2161
Aramais Khachatryan <rtgkid8@gmail.com>
2020-10-20 17:05:26 +0400

    Some list collection methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
   public static void main(String[] args) {
@@ -11,5 +16,14 @@ public class Main {
     City newYork = new City("New York");
     City shanghai = new City("Shanghai");
     City paris = new City("Paris");
+
+    List<Person> people = new ArrayList(Arrays.asList(p1, p2, p3, p4, p5, p6));
+
+    people.removeIf(p -> p.getAge() > 30);
+    people.replaceAll(p -> new Person(p.getName().toUpperCase(), p.getAge()));
+    people.sort(Comparator.comparing(Person::getAge).thenComparing(Person::getName).reversed());
+
+    people.forEach(System.out::println);
+
   }
 }
