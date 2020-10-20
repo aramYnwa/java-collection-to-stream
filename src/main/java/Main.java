ba6ea35c9a637d0e763619a3b01211deaee2fee5
Aramais Khachatryan <rtgkid8@gmail.com>
2020-10-20 17:54:35 +0400

    Initial commit: some lists to do reduction on

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
 public class Main {
 
   public static void main(String[] args) {
+
+    List<Integer> ints = Arrays.asList(0, 1, 2, 3, 4, -1, -2, -3, -4);
+
+    List<Integer> ints1 = Arrays.asList(0, 1, 2, 3, 4);
+    List<Integer> ints2 = Arrays.asList(-1, -2, -3, -4);
+
+    BinaryOperator<Integer> op = (i1, i2) -> Integer.max(i1, i2);
 
   }
 }
