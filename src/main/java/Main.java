7afbf2a461cbdf74bb4299727cf4eada436fde41
Aramais Khachatryan <rtgkid8@gmail.com>
2020-10-21 17:59:29 +0400

    Added reduce function: reduce operator should be always associative

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,18 @@ import java.util.function.BinaryOperator;
 
 public class Main {
 
+  public static int reduce(
+    List<Integer> values,
+    int valueIfEmpty,
+    BinaryOperator<Integer> reduction) {
+
+    int result = valueIfEmpty;
+    for (int value : values) {
+      result = reduction.apply(result, value);
+    }
+    return result;
+  }
+
   public static void main(String[] args) {
 
     List<Integer> ints = Arrays.asList(0, 1, 2, 3, 4, -1, -2, -3, -4);
@@ -11,7 +23,20 @@ public class Main {
     List<Integer> ints1 = Arrays.asList(0, 1, 2, 3, 4);
     List<Integer> ints2 = Arrays.asList(-1, -2, -3, -4);
 
-    BinaryOperator<Integer> op = (i1, i2) -> Integer.max(i1, i2);
+    // Reduction operation should be associative!!!
+    //BinaryOperator<Integer> op = (i1, i2) -> Integer.sum(i1, i2);
+    //BinaryOperator<Integer> op = (i1, i2) -> Integer.max(i1, i2);
 
+    //Examples of non associative reduction operations.
+    //BinaryOperator<Integer> op = (i1, i2) -> i1*i1 + i2*i2;
+    BinaryOperator<Integer> op = (i1, i2) -> (i1 + i2) / 2;
+
+    int reduction1 = reduce(ints1, 0, op);
+    int reduction2 = reduce(ints2, 0, op);
+    int reduction_parallel = reduce(Arrays.asList(reduction1, reduction2), 0, op);
+    int reduction = reduce(ints, 0, op);
+
+    System.out.println("Reduction parallel : " + reduction_parallel);
+    System.out.println("Reduction : " + reduction);
   }
 }
