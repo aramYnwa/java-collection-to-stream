dc15d037fe49d5b3aaf88d1d89b25e58fe348dbc
Aramais Khachatryan <rtgkid8@gmail.com>
2020-10-22 19:07:22 +0400

    Simple stream builder example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
+import java.util.stream.Stream;
+
 public class Main {
 
   public static void main(String[] args) {
+
+    Stream.Builder<String> builder = Stream.builder();
+    builder.add("Hello").add("World").accept("!!!");
+    Stream<String> stringStream = builder.build();
+
+    stringStream.forEach(System.out::println);
 
   }
 }
